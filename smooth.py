b117a133fe59d18da52d485523dd3c9236acd1eb
vitalyvitmens <otm_@tut.by>
2022-09-09 17:07:14 +0300

    начал функцию smooth()

---
+++ smooth.py
@@ -0,0 +1,24 @@
+from math import sin
+from numpy import rad2deg
+
+
+def smooth(x, dx):
+    def inner(arg):
+        for f in reversed(x):
+            # f(x − dx), f(x) and f(x + dx)
+            arg = f(arg)
+        return arg
+
+    return inner
+
+
+def fn(sum):
+    return round(sin(rad2deg(sum)), 3)
+
+
+smoothFunc = smooth([fn], 15)
+
+print(smoothFunc(10))
+print(fn(10))
+# smoothFunc = smooth(fn(sum) => sin(rad2deg(sum)), 15);
+# smoothFunc(10) // ~ 0.438
